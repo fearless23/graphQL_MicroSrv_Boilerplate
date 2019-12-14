2e5e3ff1dc249a0ee30ed2f648a02c06047508f4
Jaspreet Singh -- fearless <jaspreetsingh2379@gmail.com>
2019-12-15 00:49:39 +0530

    Changes Summary:
    - Added Heroku procfile.
    - Added Github Workflow - Dummy Test
    - Modified package.json to include repo, author etc..
    
    Todo:
    - Add Jest Tests

--- src/GraphQL/_graphQL.config.js
+++ src/GraphQL/_graphQL.config.js
@@ -2,7 +2,6 @@ const { typeDefs } = require("./Schema/_schema");
 const { resolvers } = require("./Resolvers/_resolvers");
 
 const context = function(x) {
-  // console.log(x.req.headers['x-auth-key'])
   return { req: x.req, res: x.res };
 };
 
--- src/server.js
+++ src/server.js
@@ -5,6 +5,40 @@ const server = new ApolloServer(graphQL_Config);
 server
   .listen()
   .then(({ url }) => console.log(`Server started at ${url}`))
-  .catch(err => {
-    console.log(error);
-  });
+  .catch(err => console.log(err));
+
+/*
+  // More Options
+  {
+    tracing,
+    cache,
+    cacheControl,
+    dataSources,
+    debug,
+    engine,
+    executor,
+    extensions,
+    fieldResolver,
+    formatError,
+    formatResponse,
+    gateway,
+    introspection,
+    mockEntireSchema,
+    mocks,
+    modules,
+    onHealthCheck,
+    parseOptions,
+    persistedQueries,
+    playground,
+    plugins,
+    resolvers,
+    rootValue,
+    schema,
+    schemaDirectives,
+    subscriptions,
+    typeDefs,
+    uploads,
+    validationRules
+  }
+
+*/
