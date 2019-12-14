a1f3937f33ea76488c98e5b0b9fb0e55f2511b1b
Jaspreet Singh -- fearless <jaspreetsingh2379@gmail.com>
2019-12-15 01:10:00 +0530

    Change Summary:
    - Modified src/server.js to fix playground disabled in production.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const { ApolloServer } = require("apollo-server");
 const { graphQL_Config } = require("./GraphQL/_graphQL.config");
 
-const server = new ApolloServer(graphQL_Config);
+const server = new ApolloServer({
+  ...graphQL_Config,
+  introspection: true,
+  playground: true
+});
 server
   .listen({ port: process.env.PORT || 4000 })
   .then(({ url }) => console.log(`Server started at ${url}`))
